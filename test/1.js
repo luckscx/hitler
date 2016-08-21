9f2a17b57d761e47235eca914f3b2c085527e691
grissomshen(沈陈霄) <grissomshen@tencent.com>
2016-08-21 14:42:29 +0800

    add room and user

--- test/1.js
+++ test/1.js
@@ -2,27 +2,42 @@
 
 // open room , join , start game
 
+const should = require('should/as-function');
+const Room = require('../lib/room');
+const User = require('../lib/user');
 
-const roomMgr = require('../lib/roomMgr');
 
 
 var assert = require('assert');
 describe('room', function() {
-  const user_id = 123;
+  const owner = new User('grissom');
   const config = {
     player_num : 5
   };
-  const room_one = roomMgr.new(user_id,config);
+  const room_one = new Room(owner,config);
+  const room_two = new Room(owner,config);
   console.log(room_one);
   describe('#new', function() {
-    it('check params', () => {
-      assert.equal(room_one.own_id,user_id);
+    it('check room_id', () => {
+      should(room_two.room_id).be.exactly(2);
+      should(room_one.room_id).be.exactly(1);
     });
     it('start false', () => {
-      assert.equal(room_one.start(),false);
+      should(room_one.start()).be.exactly(false);
     });
     it('join', () => {
-      assert.equal(room_one.start(),false);
+      const echo = new User('Echo');
+      const alec = new User('Alec');
+      const pan = new User('Pan');
+      const pinky = new User('Pinky');
+      room_one.join(echo);
+      room_one.join(alec);
+      room_one.join(pan);
+      room_one.join(pinky);
+      should(room_one.room_player).containDeep([owner,echo,alec,pan,pinky]);
+    });
+    it('start ok', () => {
+      should(room_one.start()).be.exactly(true);
     });
   });
 });
